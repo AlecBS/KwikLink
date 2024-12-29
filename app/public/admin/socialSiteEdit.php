e29b4e6e1ad406521c7ac0f4b3c05a3143ba65dd
Alec Sherman <alec.sherman@me.com>
2024-12-28 21:12:49 -0800

    socialSite List and Edit pages

---
+++ app/public/admin/socialSiteEdit.php
@@ -0,0 +1,51 @@
+<?PHP
+$pgSecurityLevel = 50;
+if (!isset($gloConnected)):
+    define('_RootPATH', '../');
+    require('../wtk/wtkLogin.php');
+endif;
+
+$pgSQL =<<<SQLVAR
+SELECT `UID`, `WebsiteName`, `IconHTML`
+  FROM `SocialSites`
+WHERE `UID` = ?
+SQLVAR;
+$pgSQL  = wtkSqlPrep($pgSQL);
+if ($gloWTKmode != 'ADD'):
+    $gloForceRO = wtkPageReadOnlyCheck('/admin/socialSiteEdit.php', $gloId);
+    wtkSqlGetRow($pgSQL, [$gloId]);
+endif;
+
+$pgHtm =<<<htmVAR
+<div class="container">
+    <h4>Social Media Sites</h4><br>
+    <div class="card content b-shadow">
+        <form id="wtkForm" name="wtkForm" method="POST">
+            <span id="formMsg" class="red-text">$gloFormMsg</span>
+            <div class="row">
+htmVAR;
+
+$pgHtm .= wtkFormText('SocialSites', 'WebsiteName');
+if ($gloWTKmode == 'Copy'): // Copy data feature
+    $pgHtm = wtkReplace($pgHtm, ' name="Origwtk', ' name="Copywtk');
+    $gloWTKmode = 'ADD';
+endif;
+$pgHtm .= wtkFormText('SocialSites', 'IconHTML');
+
+$pgHtm .= wtkFormHidden('ID1', $gloId);
+$pgHtm .= wtkFormHidden('UID', wtkEncode('UID'));
+$pgHtm .= wtkFormHidden('wtkMode', $gloWTKmode);
+$pgHtm .= wtkFormHidden('wtkGoToURL', '../../admin/socialSiteList.php');
+//$pgHtm .= wtkFormPrimeField('SocialSites', 'ParentUID', $gloRNG);
+$pgHtm .= '            </div>' . "\n";
+$pgHtm .= wtkUpdateBtns() . "\n";
+$pgHtm .= wtkFormWriteUpdField();
+
+$pgHtm .=<<<htmVAR
+        </form>
+    </div>
+</div>
+htmVAR;
+echo $pgHtm;
+exit;
+?>
---
+++ app/public/admin/socialSiteList.php
@@ -0,0 +1,74 @@
+<?PHP
+$pgSecurityLevel = 50;
+if (!isset($gloConnected)):
+    define('_RootPATH', '../');
+    require('../wtk/wtkLogin.php');
+endif;
+
+$pgSQL =<<<SQLVAR
+SELECT `UID`, `WebsiteName`, `IconHTML`
+  FROM `SocialSites`
+WHERE `DelDate` IS NULL
+SQLVAR;
+$pgHideReset = ' class="hide"';
+$pgFilterValue = wtkFilterRequest('wtkFilter');
+if ($pgFilterValue != ''):
+    $pgSQL .= " AND lower(`WebsiteName`) LIKE lower('%" . $pgFilterValue . "%')" . "\n";
+    $pgHideReset = '';
+endif;  // $pgFilterValue != ''
+
+$pgSQL .= ' ORDER BY `WebsiteName` ASC';
+
+$gloEditPage = 'socialSiteEdit';
+$gloAddPage  = $gloEditPage;
+$gloDelPage  = 'SocialSitesDelDate'; // have DelDate at end if should DelDate instead of DELETE
+
+// If you want phone version to show less columns...
+// if ($gloDeviceType == 'phone'):
+//     $pgSQL = wtkReplace($pgSQL, ', `ExtraColumns`','');
+// endif;
+
+// put in columns you want sortable here:
+//wtkSetHeaderSort('ColumnName', 'Column Header');
+//wtkFillSuppressArray('ColumnName');
+
+//$gloColumnAlignArray = array (
+//    'Priority'   => 'center'
+//);
+
+/*
+$gloMoreButtons = array(
+    'User Logins' => array(
+            'act' => 'pageName',
+            'img' => 'arrow-right'
+            )
+    );
+*/
+
+$pgHtm  = '<div class="container">' . "\n";
+$pgHtm .= '    <h4>Social Media Sites</h4><br>' . "\n";
+$pgHtm .=<<<htmVAR
+    <h5> Website Name Quick Filters <small id="filterReset"$pgHideReset>
+        <button onclick="JavaScript:wtkBrowseReset('socialSiteList','SocialSites','$gloRNG')" type="button" class="btn btn-small btn-save waves-effect waves-light right">Reset List</button>
+        </small>
+    </h5>
+    <form method="post" name="wtkFilterForm" id="wtkFilterForm" role="search" class="wtk-search card b-shadow">
+        <input type="hidden" id="Filter" name="Filter" value="Y">
+        <div class="input-field">
+           <div class="filter-width">
+              <input value="$pgFilterValue" name="wtkFilter" id="wtkFilter" type="search" placeholder="enter partial value to search for">
+           </div>
+           
+           <button onclick="Javascript:wtkBrowseFilter('socialSiteList','SocialSites')" id="wtkFilterBtn" type="button" class="btn waves-effect waves-light"><i class="material-icons">search</i></button>
+        </div>
+    </form>
+htmVAR;
+
+$pgHtm .= '    <div class="wtk-list card b-shadow">' . "\n";
+$pgHtm .= wtkBuildDataBrowse($pgSQL, [], 'SocialSites', '/admin/socialSiteList.php');
+//$pgHtm  = wtkReplace($pgHtm, 'There is no data available.','no users yet');
+$pgHtm .= '</div></div>' . "\n";
+
+echo $pgHtm;
+exit;
+?>
