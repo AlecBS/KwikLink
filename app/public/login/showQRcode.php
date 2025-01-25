782d737b09351cd9e9dc5e228023b66804901538
Alec Sherman <alec.sherman@me.com>
2025-01-24 19:41:16 -0800

    minor changes to showQRcode.php so builds URL based on user's ID custom version of WTK ajxRegister.php so upon registration it shows the User Edit page immediately instead of empty Dashboard

--- app/public/login/showQRcode.php
+++ app/public/login/showQRcode.php
@@ -3,6 +3,7 @@ $pgSecurityLevel = 1;
 define('_RootPATH', '../');
 require('../wtk/wtkLogin.php');
 
+/*
 $pgSQL =<<<SQLVAR
 SELECT `PersonalURL`
  FROM `wtkUsers`
@@ -10,8 +11,11 @@ WHERE `UID` = ?
 SQLVAR;
 $pgSQL = wtkSqlPrep($pgSQL);
 $pgURL = wtkSqlGetOneResult($pgSQL, [$gloUserUID]);
+*/
 
-$pgQRimage  = '<img src="makeQRcode.php?pw=LowCodeOrDie&url=' . $pgURL . '"';
+$pgURL = $gloWebBaseURL . '/card.php?id=' . $gloUserUID ;
+
+$pgQRimage  = '<img src="/login/makeQRcode.php?pw=LowCodeOrDie&url=' . $pgURL . '"';
 $pgQRimage .= ' class="responsive-img transparent">';
 
 $pgHtm = '<h3 class="center">Scan QR Code</h3><br>' . "\n";
